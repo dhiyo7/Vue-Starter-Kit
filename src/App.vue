ee29d730aa36672f602d08b7c2d20d71d68fee3e
dhiyo7 <dhiyoreksa12@gmail.com>
2023-03-07 01:27:35 +0700

    [DHIYO][ADD] - add sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
-<script setup>
-import {RouterView} from "vue-router";
+<script>
 import SideBar from "@/components/molecules/sidebar/index.vue"
+
+export default {
+  components: {
+    SideBar
+  },
+  data() {
+    return {
+      navOpen: false
+    }
+  },
+  methods: {
+    setOpen(newVal) {
+      console.log("FROM CHILD ", newVal)
+      this.navOpen = newVal
+    }
+  }
+}
 </script>
 <template>
-  <div class="flex h-screen">
-    <SideBar />
-    <div class="flex-initial w-full p-10">
+  <div class="flex">
+    <SideBar :setOpen="setOpen"/>
+    <div :class="['flex-initial w-full p-10' , !navOpen ? 'ml-0' : 'ml-52']">
       <RouterView/>
     </div>
   </div>
